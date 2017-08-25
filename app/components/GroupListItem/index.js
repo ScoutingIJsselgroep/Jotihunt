878f21bc1c232247cd847f64d89b6e029a9d1220
Tristan de Boer <info@tristandeboer.nl>
2017-08-25 12:41:42 +0200

    Added MapViewer

--- app/components/GroupListItem/index.js
+++ app/components/GroupListItem/index.js
@@ -10,13 +10,13 @@ import styled from 'styled-components';
 import {FormattedMessage} from 'react-intl';
 import { Link } from 'react-router';
 import messages from './messages';
+import Gpsbutton from 'components/Gpsbutton';
 
 const Label = styled.span`
   background-color: ${props => props.color} !important;
 `;
 
 function GroupListItem({group}) {
-  console.log(group);
   return (
     <tr>
       <td>
@@ -35,8 +35,8 @@ function GroupListItem({group}) {
         <div className="btn-group" role="group">
           <Link to={'/map/' + group.latitude + '/' + group.longitude} className="btn btn-default"><i
             className="fa fa-map-o" aria-hidden="true"></i> Toon op kaart</Link>
-          <Link to={'geo:' + group.latitude + ',' + group.longitude + ';u=35'} className="btn btn-default"><i
-            className="fa fa-location-arrow" aria-hidden="true"></i> GPS</Link>
+
+          <Gpsbutton latitude={group.latitude} longitude={group.longitude} />
         </div>
       </td>
     </tr>

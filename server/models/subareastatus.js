a07aca8dc15fd31e8f3b992550cc9c7db34425fa
Tristan de Boer <info@tristandeboer.nl>
2017-10-05 18:10:55 +0200

    Added StatusBar

--- server/models/subareastatus.js
+++ server/models/subareastatus.js
@@ -6,10 +6,10 @@ module.exports =  function (sequelize, DataTypes) {
   });
 
   SubareaStatus.getLatest = function (callback) {
-    sequelize.query("SELECT s1.* "+
-    "FROM SubareaStatuses s1 LEFT JOIN SubareaStatuses s2 "+
+    sequelize.query("SELECT s1.*, Statuses.status, Subareas.name, Subareas.color, Statuses.statuscolor "+
+    "FROM Statuses, Subareas, SubareaStatuses s1 LEFT JOIN SubareaStatuses s2 "+
     "ON (s1.SubareaId = s2.SubareaId AND s1.id < s2.id) "+
-    "WHERE s2.id IS NULL;").then(results => callback(results[0]));
+    "WHERE s2.id IS NULL AND Statuses.id = s1.StatusId AND Subareas.id = s1.SubareaId;").then(results => callback(results[0]));
   };
 
   SubareaStatus.associate = function (models) {

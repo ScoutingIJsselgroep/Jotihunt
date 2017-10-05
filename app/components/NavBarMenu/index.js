19c454f56bb544583f7fcbb51878ee3a70321569
Tristan de Boer <info@tristandeboer.nl>
2017-10-05 18:18:58 +0200

    Added menu bar entry

--- app/components/NavBarMenu/index.js
+++ app/components/NavBarMenu/index.js
@@ -21,6 +21,7 @@ class NavBarMenu extends React.Component { // eslint-disable-line react/prefer-s
           <li><Link to="/hint/add">Hint toevoegen</Link></li>
           <li><Link to="/hint/list">Hints <i className="fa fa-list" aria-hidden="true"></i> </Link></li>
           <li><Link to="/group/list">Groepen <i className="fa fa-list" aria-hidden="true"></i> </Link></li>
+          <li><Link to="/map">Kaart <i className="fa fa-map" aria-hidden="true"></i> </Link></li>
           <li><Link to="/about">Over</Link></li>
         </ul>
       );

77b579716bde49462fc1f2231bd04aa4a79f44a2
Tristan de Boer <info@tristandeboer.nl>
2017-10-11 01:08:14 +0200

    Added clairvoyance menu

---
+++ app/components/ClairvoyanceResultMapper/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ClairvoyanceResultMapper from '../index';
+
+describe('<ClairvoyanceResultMapper />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

92ab0b499f5974c90e985899f6eff3f60c7c6ed5
Tristan de Boer <info@tristandeboer.nl>
2017-10-17 20:53:54 +0200

    Reset rightClickLatLng on close click.

--- app/containers/MassiveMap/index.js
+++ app/containers/MassiveMap/index.js
@@ -87,6 +87,7 @@ export class MassiveMap extends React.Component { // eslint-disable-line react/p
 
     const MyMapComponent = withGoogleMap((props) =>
       <GoogleMap
+        options={{scaleControl: true}}
         defaultZoom={9}
         defaultCenter={{ lat: 52.1523337615325, lng: 5.859883117643787 }}
         onRightClick={this.onRightClick}
@@ -114,7 +115,7 @@ export class MassiveMap extends React.Component { // eslint-disable-line react/p
         />
         <MyMapComponent
           isMarkerShown
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwcf9GYLw_Q3Rm92D6KQ2k1pilgqTAjC0v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: '100%' }} />}
           containerElement={<div style={{ height: '600px' }} />}
           mapElement={<div style={{ height: '100%' }} />}

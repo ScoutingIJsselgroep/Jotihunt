4eb0188ded18a41f957f1895127ed2fe9df28b86
Tristan de Boer <info@tristandeboer.nl>
2018-10-02 12:14:32 +0200

    Changed length of Content in Database

--- server/models/api.js
+++ server/models/api.js
@@ -3,7 +3,7 @@ module.exports = function (sequelize, DataTypes) {
   var Api = sequelize.define("Api", {
     messageId: DataTypes.STRING,
     title: DataTypes.STRING,
-    content: DataTypes.STRING,
+    content: DataTypes.STRING(5000),
     start: DataTypes.DATE,
     end: DataTypes.DATE,
     points: DataTypes.INTEGER

8e2c4b12c7e31d67f89713566ad282de43dfcd9d
Tristan de Boer <info@tristandeboer.nl>
2018-10-04 22:10:38 +0200

    Added setenvironment.sh

---
+++ setenvironment.sh
@@ -0,0 +1,7 @@
+export AUTH0_CLIENT_ID=''
+export AUTH0_DOMAIN=''
+export NODE_ENV=''
+export TELEGRAM_TOKEN=''
+export GOOGLE_AUTH_TOKEN=''
+export POLLER_INTERVAL_SECONDS=61
+export KML_FILENAME='jotihunt_2017.kml'

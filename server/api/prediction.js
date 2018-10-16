8403540ec45f9b400b50527d79095509adafbaa2
Tristan de Boer <info@tristandeboer.nl>
2018-10-16 11:48:29 +0200

    Removed unused code

--- server/api/prediction.js
+++ server/api/prediction.js
@@ -32,61 +32,6 @@ function getLastHints(callback) {
   });
 }
 
-/*
-function getNextLocation(callback) {
-  models.Group.findAll({
-    include: [models.Subarea],
-  }).then((groups) => {
-    // Group by subarea
-    const groupedGroups = _.groupBy(groups, 'Subarea.name');
-
-    // Group by number of visits
-    for (let key in groupedGroups) {
-      groupedGroups[key] = _.groupBy(groupedGroups[key], 'visits');
-    }
-
-    // Loop every subarea
-    _.map(groupedGroups, (subareaGroups) => { // Loops 6 times
-      // Loop every groups with same visits
-      _.map(subareaGroups, (sameVisitsGroups) => {
-        // Generate shortest path between groups that have the same amount
-        // of visits
-
-        // Setup Google Direction Services
-        const googleMapsClient = require('@google/maps').createClient({
-          key: config.google.googleAppId,
-        });
-
-
-        // Loop sameVisitsGroups as destinations
-        _.map(sameVisitsGroups, (destinationGroup) => {
-
-          // Set other groups with same number of visits but destinationGroup as waypoints
-          const waypoints = _.map(_.reject(sameVisitsGroups, destinationGroup), (group) => [group.latitude, group.longitude]);
-          console.log(waypoints);
-          // Get shortest from groups to destination
-          googleMapsClient.directions({
-            origin: [52.2, 6.2],
-            destination: [destinationGroup.latitude, destinationGroup.longitude],
-            waypoints: waypoints, // Everything but destinationGroup
-            optimize: true,
-            mode: "walking",
-          }, (err, response) => {
-            console.log("ROUTEEEE==============");
-            console.log(response.json.routes[0]);
-          });
-        });
-
-
-        // Return groups as waypoints and add every group (but last) to next visit category
-      });
-    });
-
-    // console.log(groupedGroups);
-  });
-}*/
-
-// TODO: Add caching function
 /**
   Perform a call to Projection, a location prediction API.
 */

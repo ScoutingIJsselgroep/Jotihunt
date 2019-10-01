d9b07647108369f9cdee26676cdacb0333fbfefa
Tristan de Boer <info@tristandeboer.nl>
2019-10-01 21:55:15 +0200

    Removed Uglify

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -17,7 +17,7 @@ module.exports = require('./webpack.base.babel')({
     chunkFilename: '[name].[chunkhash].chunk.js',
   },
 
-  plugins: [
+  plugins: [z
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
       children: true,
@@ -25,27 +25,9 @@ module.exports = require('./webpack.base.babel')({
       async: true,
     }),
 
-    // Minify and optimize the index.html
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-      inject: true,
-    }),
-
     new WebpackPwaManifest({
-      name: 'Jotihunt 2018',
-      short_name: 'JH18',
+      name: 'Jotihunt 2019',
+      short_name: 'JH19',
       description: 'Dashboard voor Jotihunt van Scouting Gorssel!',
       background_color: '#fafafa',
       theme_color: '#ef5800',

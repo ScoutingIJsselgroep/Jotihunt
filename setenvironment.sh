30b176aeae6b8bfc2c1e6feac374635a25430276
Tristan de Boer <info@tristandeboer.nl>
2021-10-11 17:51:38 +0200

    Delete setenvironment.sh

--- setenvironment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export AUTH0_CLIENT_ID=KxwFZwd44I4ERX2dBV0dsO8wPPJ5oNaZ
-export AUTH0_DOMAIN="jotihunt-js.eu.auth0.com"
-# export NODE_ENV="production"
-export TELEGRAM_TOKEN="122155087:AAGEGn5RJgzUEq3IMNS8TLQLUjr1lZwxUEc"
-export GOOGLE_AUTH_TOKEN="AIzaSyATLQHsOrwWkL_CNba3OsdTtKlk8Z19Pms"
-export POLLER_INTERVAL_SECONDS=61
-export KML_FILENAME=jotihunt2018.kml

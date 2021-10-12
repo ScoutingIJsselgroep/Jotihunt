191deb0b7ceadfa554960a3284d10ac7d4ec3b67
Tristan de Boer <info@tristandeboer.nl>
2021-10-12 21:23:57 +0200

    Add the double amount of groups

--- app/components/NewHintForm/index.js
+++ app/components/NewHintForm/index.js
@@ -47,31 +47,60 @@ class NewHintForm extends React.Component { // eslint-disable-line react/prefer-
                 <Select
                   className="form-control"
                   field='subarea'
-                  options={[{
-                    label: 'Alpha',
-                    value: 'Alpha',
-                    key: 1
-                  }, {
-                    label: 'Bravo',
-                    value: 'Bravo',
-                    key: 2
-                  }, {
-                    label: 'Charlie',
-                    value: 'Charlie',
-                    key: 3
-                  }, {
-                    label: 'Delta',
-                    value: 'Delta',
-                    key: 4
-                  }, {
-                    label: 'Echo',
-                    value: 'Echo',
-                    key: 5
-                  }, {
-                    label: 'Foxtrot',
-                    value: 'Foxtrot',
-                    key: 6
-                  }]}
+                  options={[
+                    {
+                      label: 'Alpha',
+                      value: 'Alpha',
+                      key: 0
+                    }, {
+                      label: 'Bravo',
+                      value: 'Bravo',
+                      key: 1
+                    }, {
+                      label: 'Charlie',
+                      value: 'Charlie',
+                      key: 2
+                    }, {
+                      label: 'Delta',
+                      value: 'Delta',
+                      key: 3
+                    }, {
+                      label: 'Echo',
+                      value: 'Echo',
+                      key: 4
+                    }, {
+                      label: 'Foxtrot',
+                      value: 'Foxtrot',
+                      key: 5
+                    }, {
+                      label: 'n.n.b.',
+                      value: 'n.n.b.',
+                      key: 6
+                    }, {
+                      label: 'Alpha 2',
+                      value: 'Alpha 2',
+                      key: 7
+                    }, {
+                      label: 'Bravo 2',
+                      value: 'Bravo 2',
+                      key: 8
+                    }, {
+                      label: 'Charlie 2',
+                      value: 'Charlie 2',
+                      key: 9
+                    }, {
+                      label: 'Delta 2',
+                      value: 'Delta 2',
+                      key: 10
+                    }, {
+                      label: 'Echo 2',
+                      value: 'Echo 2',
+                      key: 11
+                    }, {
+                      label: 'Foxtrot 2',
+                      value: 'Foxtrot 2',
+                      key: 12
+                    }]}
                 />
               </div>
               <button type="submit" className="btn btn-default">Versturen</button>
--- app/containers/AddHunt/index.js
+++ app/containers/AddHunt/index.js
@@ -80,12 +80,18 @@ export class AddHunt extends React.Component { // eslint-disable-line react/pref
                   <div className="form-group">
                     <span>Deelgebied</span>
                     {this.props.subarea ? <select className="form-control" ref="subarea" disabled={this.props.huntResult ? true : false} value={this.props.subarea}>
-                      <option value="Alpha">Alpha</option>
+                    <option value="Alpha">Alpha</option>
                       <option value="Bravo">Bravo</option>
                       <option value="Charlie">Charlie</option>
                       <option value="Delta">Delta</option>
                       <option value="Echo">Echo</option>
                       <option value="Foxtrot">Foxtrot</option>
+                      <option value="Alpha 2">Alpha 2</option>
+                      <option value="Bravo 2">Bravo 2</option>
+                      <option value="Charlie 2">Charlie 2</option>
+                      <option value="Delta 2">Delta 2</option>
+                      <option value="Echo 2">Echo 2</option>
+                      <option value="Foxtrot 2">Foxtrot 2</option>
                     </select> :
                     <select className="form-control" ref="subarea" disabled={this.props.huntResult ? true : false} value={this.props.params.subarea}>
                       <option value="Alpha">Alpha</option>
@@ -94,6 +100,12 @@ export class AddHunt extends React.Component { // eslint-disable-line react/pref
                       <option value="Delta">Delta</option>
                       <option value="Echo">Echo</option>
                       <option value="Foxtrot">Foxtrot</option>
+                      <option value="Alpha 2">Alpha 2</option>
+                      <option value="Bravo 2">Bravo 2</option>
+                      <option value="Charlie 2">Charlie 2</option>
+                      <option value="Delta 2">Delta 2</option>
+                      <option value="Echo 2">Echo 2</option>
+                      <option value="Foxtrot 2">Foxtrot 2</option>
                     </select>
                     }
                   </div>

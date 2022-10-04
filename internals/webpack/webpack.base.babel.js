6f0d281a7077fce387c16be87c973063f71cb435
Tristan de Boer <info@tristandeboer.nl>
2022-10-04 19:59:37 +0200

    Save quality as array

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -13,62 +13,63 @@ module.exports = (options) => ({
   }, options.output), // Merge with env dependent settings
   module: {
     loaders: [{
-      test: /\.js$/, // Transform all .js files required somewhere with Babel
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: options.babelQuery,
-    }, {
-      // Do not transform vendor's CSS with CSS-modules
-      // The point is that they remain in global scope.
-      // Since we require these CSS files in our JS or CSS files,
-      // they will be a part of our compilation either way.
-      // So, no need for ExtractTextPlugin here.
-      test: /\.css$/,
-      include: /node_modules/,
-      loaders: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      loaders: ['style-loader', 'css-loader', 'sass-loader'],
-    },
-    {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'file-loader',
-    }, {
-      test: /\.(jpg|png|gif)$/,
-      loaders: [
-        'file-loader',
-        {
-          loader: 'image-webpack-loader',
-          query: {
-            progressive: true,
-            optimizationLevel: 7,
-            interlaced: false,
-            pngquant: {
-              quality: '65-90',
-              speed: 4,
+        test: /\.js$/, // Transform all .js files required somewhere with Babel
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: options.babelQuery,
+      }, {
+        // Do not transform vendor's CSS with CSS-modules
+        // The point is that they remain in global scope.
+        // Since we require these CSS files in our JS or CSS files,
+        // they will be a part of our compilation either way.
+        // So, no need for ExtractTextPlugin here.
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+      }, {
+        test: /\.(jpg|png|gif)$/,
+        loaders: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            query: {
+              progressive: true,
+              optimizationLevel: 7,
+              interlaced: false,
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
             },
           },
-        },
-      ],
-    }, {
-      test: /\.kml$/,
-      loader: 'raw-loader',
-    },
-    {
-      test: /\.html$/,
-      loader: 'html-loader',
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader',
-    }, {
-      test: /\.(mp4|webm)$/,
-      loader: 'url-loader',
-      query: {
-        limit: 10000,
+        ],
+      }, {
+        test: /\.kml$/,
+        loader: 'raw-loader',
       },
-    }],
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader',
+      }, {
+        test: /\.(mp4|webm)$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+        },
+      }
+    ],
   },
   plugins: options.plugins.concat([
     new webpack.ProvidePlugin({
